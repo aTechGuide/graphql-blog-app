998a0d63cee7daf7429f949384f6bb083b79791a
Kamran Ali <kamali@expedia.com>
2019-10-10 06:58:42 +0530

    Updating subscriptions to use prisma

--- src/resolvers/subscription.js
+++ src/resolvers/subscription.js
@@ -1,19 +1,27 @@
 const Subscription = {
   comment: {
-    subscribe(parent, {postId}, {pubsub, db}, info) {
-      const post = db.posts.find((post) => post.id === postId && post.published)
+    subscribe(parent, {postId}, {prisma}, info) {
 
-      if(!post) {
-        throw new Error('Post not Found')
-      }
-
-      return pubsub.asyncIterator(`comment ${postId}`) 
-
+      return prisma.subscription.comment({
+        where : {
+          node: {
+            post: {
+              id: postId
+            }
+          }
+        }
+      }, info)
     }
   },
   post: {
-    subscribe(parent, args, {pubsub}, info) {
-      return pubsub.asyncIterator('post') 
+    subscribe(parent, args, {prisma}, info) {
+      return prisma.subscription.post({
+        where : {
+          node: {
+            published: true
+          }
+        }
+      }, info)
     }
   }
 }
