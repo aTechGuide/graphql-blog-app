b9171e8aca7a573e0ecb7a5aec42b2981e3ecd15
Kamran Ali <kamali@expedia.com>
2019-10-12 07:01:35 +0530

    Setting token expiration

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import getUserId from '../utils/getUserId';
+import generateToken from '../utils/generateToken';
 
 const Mutation = {
   async createUser(parent, args, {prisma}, info) {
@@ -27,7 +28,7 @@ const Mutation = {
     
     return {
       user,
-      token: jwt.sign({userId: user.id}, 'thisisasecret')
+      token: generateToken(user.id)
     }
   },
   async login(parent, args, {prisma}, info) {
@@ -50,7 +51,7 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({userId: user.id}, 'thisisasecret')
+      token: generateToken(user.id)
     }
   },
   async deleteUser(parent, args, {prisma, request}, info) {
