edaa773effef9f9026949e4b50b43eb313c48840
Kamran Ali <kamranalinitb@gmail.com>
2019-10-16 06:28:23 +0530

    Testing: Querying author and post

--- tests/user.test.js
+++ tests/user.test.js
@@ -79,3 +79,49 @@ test('Should create a new User', async () => {
   expect(exists).toBe(true)
 
 })
+
+test('Should expose public author profile', async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `
+
+  const response = await client.query({
+    query: getUsers
+  })
+
+  const users = response.data.users
+  expect(users.length).toBe(1)
+  expect(users[0].email).toBe(null)
+  expect(users[0].name).toBe('Jen')
+
+})
+
+test('Should expose public posts', async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `
+
+  const response = await client.query({
+    query: getPosts
+  })
+
+  const posts = response.data.posts
+  expect(posts.length).toBe(1)
+  expect(posts[0].title).toBe('Test Post 1')
+  expect(posts[0].body).toBe('Test Published Post 1 Body')
+  expect(posts[0].published).toBe(true)
+
+})
